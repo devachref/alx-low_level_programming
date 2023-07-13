9c7a11dae388ee79b43c413ee77dd2134dda167a
devachref <bennadjiachref5@gmail.com>
2023-07-13 14:04:11 -0700

    send

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+
+/**
+ *
+ * hello
+ *
+ * djhqkjlhdjkq
+*/
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
