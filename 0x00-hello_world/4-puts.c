ad42f64d11c3f02e9489528da9e912adebb4852f
devachref <bennadjiachref5@gmail.com>
2023-07-13 13:46:59 -0700

    send

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriptin: A C program that prints with put fuction 
+ *
+ * Return: Alawysd 0 (Success)
+ * */
+
+
 int main(void)
 {
-       	puts("\"Programming is like building a multilingual puzzle:");
+       	puts("\"Programming is like building a multilingual puzzle");
        	return (0);
 }
