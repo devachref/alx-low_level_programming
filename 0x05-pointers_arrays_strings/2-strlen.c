d1d5b385df0ac605d79b01e1e502583c109f8931
devachref <bennadjiachref5@gmail.com>
2023-07-24 09:09:49 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	size_t length = strlen(*s);
+
+	return (length);
+}
